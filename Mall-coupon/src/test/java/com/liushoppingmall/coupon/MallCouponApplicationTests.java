e93cd47d6f928b94528d850dbe23c1f128641d5a
yaliu777 <yaliu20160710@gmail.com>
2023-08-01 11:37:33 -0400

    initialize project structure

---
+++ Mall-coupon/src/test/java/com/liushoppingmall/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liushoppingmall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Mall-warehouse/src/main/java/com/liushoppingmall/warehouse/MallWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.liushoppingmall.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallWarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallWarehouseApplication.class, args);
+    }
+
+}
